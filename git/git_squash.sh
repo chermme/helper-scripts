f04aa42328368264acad0435b981ef402ee2de40
Peter Chermanowicz <ch1_git_762e4c1c@eml.cc>
2022-02-09 10:44:42 +0000

    Tiny fix to git_squash

--- git/git_squash.sh
+++ git/git_squash.sh
@@ -7,7 +7,7 @@ CURRENT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
 # Make sure we don't make a backup branch repeatedly
 if [[ $CURRENT_BRANCH_NAME == backup* ]]; then
-	>&2 echo "${YELLOW}Branch '"$CURRENT_BRANCH_NAME"' is already a backup${NC}"
+	echo >&2 "${YELLOW}Branch '"$CURRENT_BRANCH_NAME"' is already a backup${NC}"
 	exit 1
 fi
 
@@ -16,7 +16,6 @@ BACKUP_BRANCH_NAME="backup/$(date +%Y-%m-%d__%H-%M-%S)/$CURRENT_BRANCH_NAME"
 git branch $BACKUP_BRANCH_NAME
 
 msg=${1:-Squash Commit}
-backup-branch
-git reset --soft HEAD~$(git rev-list --count HEAD ^master);
-git add .;
-git commit -m "$msg";
+git reset --soft HEAD~$(git rev-list --count HEAD ^master)
+git add .
+git commit -m "$msg"
