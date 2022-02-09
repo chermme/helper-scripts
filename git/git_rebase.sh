c0f5d8e94abc33e64db674b69d11ffc4e41083ea
Peter Chermanowicz <ch1_git_762e4c1c@eml.cc>
2022-02-09 10:23:06 +0000

    Rebase against parent branch

--- git/git_rebase.sh
+++ git/git_rebase.sh
@@ -4,10 +4,11 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 CURRENT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+PARENT_BRANCH_NAME=$(git show-branch | grep '*' | grep -v "$(git rev-parse --abbrev-ref HEAD)" | head -n1 | sed 's/.*\[\(.*\)\].*/\1/' | sed 's/[\^~].*//')
 
 # Make sure we don't make a backup branch repeatedly
 if [[ $CURRENT_BRANCH_NAME == backup* ]]; then
-	>&2 echo "${YELLOW}Branch '"$CURRENT_BRANCH_NAME"' is already a backup${NC}"
+	echo >&2 "${YELLOW}Branch '"$CURRENT_BRANCH_NAME"' is already a backup${NC}"
 	exit 1
 fi
 
@@ -16,6 +17,6 @@ BACKUP_BRANCH_NAME="backup/$(date +%Y-%m-%d__%H-%M-%S)/$CURRENT_BRANCH_NAME"
 git branch $BACKUP_BRANCH_NAME
 
 echo "${YELLOW}Current branch has been backed up as: $BACKUP_BRANCH_NAME${NC}"
-echo "${YELLOW}Rebasing onto master...${NC}"
+echo "${YELLOW}Rebasing onto parent branch $PARENT_BRANCH_NAME...${NC}"
 
-git pull origin master --rebase
+git pull origin $PARENT_BRANCH_NAME --rebase
