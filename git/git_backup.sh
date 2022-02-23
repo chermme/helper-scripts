61641f3fbb8b1c237f7a9badcd7e2cd21f48db39
Peter Chermanowicz <ch1_git_762e4c1c@eml.cc>
2022-02-23 09:43:16 +0000

    Tiny change to the backup branch names

--- git/git_backup.sh
+++ git/git_backup.sh
@@ -7,11 +7,11 @@ CURRENT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
 # Make sure we don't make a backup branch repeatedly
 if [[ $CURRENT_BRANCH_NAME == backup* ]]; then
-	>&2 echo "${YELLOW}Branch '"$CURRENT_BRANCH_NAME"' is already a backup${NC}"
+	echo >&2 "${YELLOW}Branch '"$CURRENT_BRANCH_NAME"' is already a backup${NC}"
 	exit 1
 fi
 
-BACKUP_BRANCH_NAME="backup/$(date +%Y-%m-%d__%H-%M-%S)/$CURRENT_BRANCH_NAME"
+BACKUP_BRANCH_NAME="backup/$(date +%Y-%m-%d/%H-%M-%S)/$CURRENT_BRANCH_NAME"
 
 git branch $BACKUP_BRANCH_NAME
 
