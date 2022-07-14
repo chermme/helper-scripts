0f96fbbea0fac1d41530a91fcf7caeda15f05ad2
Peter Chermanowicz <ch1_git_762e4c1c@eml.cc>
2022-07-14 11:08:10 +0100

    set-env

---
+++ anyjunk/set-env.sh
@@ -0,0 +1,14 @@
+if [ ! $1 ]; then
+  ENV="staging"
+else
+  ENV=$1
+fi
+
+export API_URL="${ENV}-hyrax.api-anyjunk.co.uk"
+export API_SEARCH_URL="${ENV}-hyrax.api-anyjunk.co.uk",
+
+echo " "
+echo "ENV vars set to:"
+echo "API_URL: ${ENV}-hyrax.api-anyjunk.co.uk"
+echo "API_SEARCH_URL: ${ENV}-hyrax.api-anyjunk.co.uk"
+echo " "
