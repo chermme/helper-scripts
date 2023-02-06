df9d9bd0883730505350c1797e154791855f7e11
Peter Chermanowicz <peter@cherm.me>
2023-02-06 10:08:51 +0000

    set-env api urls updated

--- anyjunk/set-env.sh
+++ anyjunk/set-env.sh
@@ -4,8 +4,8 @@ else
   ENV=$1
 fi
 
-export API_URL="${ENV}-hyrax.api-anyjunk.co.uk"
-export API_SEARCH_URL="${ENV}-hyraxsearch.api-anyjunk.co.uk"
+export API_URL="${ENV}-hyrax.anyjunk.co.uk"
+export API_SEARCH_URL="${ENV}-hyraxsearch.anyjunk.co.uk"
 
 echo " "
 echo "ENV vars set to:"
