8e193210a09b0f7fe4fa0f5ef8d48f4aa52bb04c
Peter Chermanowicz <petercherm@digitaloop.com>
2024-03-12 17:32:36 +0000

    set-env sets both V1 and V2 env vars

--- anyjunk/set-env.sh
+++ anyjunk/set-env.sh
@@ -7,8 +7,15 @@ fi
 export API_URL="${ENV}-hyrax.api-hss.com"
 export API_SEARCH_URL="${ENV}-hyraxsearch.api-hss.com"
 
-echo " "
+export HSS_API_URL="${ENV}-hyrax.api-hss.com"
+export HSS_API_SEARCH_URL="${ENV}-hyraxsearch.api-hss.com"
+
+echo "------------------------------------------"
 echo "ENV vars set to:"
 echo "API_URL: ${API_URL}"
 echo "API_SEARCH_URL: ${API_SEARCH_URL}"
-echo " "
+echo "------------------------------------------"
+echo "HSS-ENV vars set to:"
+echo "HSS_API_URL: ${HSS_API_URL}"
+echo "HSS_API_SEARCH_URL: ${HSS_API_SEARCH_URL}"
+echo "------------------------------------------"
