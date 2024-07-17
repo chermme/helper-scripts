91dde47fdc79ea6661aa05b8d2246c565575c68d
Piotr Chermanowicz <pcherm@icloud.com>
2024-07-17 14:29:31 +0100

    sync-pr.sh updated

--- hss/sync-pr.sh
+++ hss/sync-pr.sh
@@ -1,10 +1,10 @@
 PR_NUMBER=$1
 
 SOURCE_REPO=thedrawingroom/hss-ux-components
-SOURCE_REPO_PATH=/Users/petercherm/git/AnyJunk/hss-ux-components
+SOURCE_REPO_PATH=$HOME/Git/tdr/hss-ux-components
 
 DEST_REPO=HSS-Proservice/react-ux
-DEST_REPO_PATH=/Users/petercherm/git/AnyJunk/react-ux
+DEST_REPO_PATH=$HOME/Git/hss/react-ux
 
 if [ -z "$PR_NUMBER" ]; then
   echo "PR_NUMBER is not provided. Exiting..."
@@ -26,5 +26,5 @@ git checkout -b sync/$PR_BRANCH_NAME
 
 for file in $CHANGED_FILES; do
   echo "Copying $SOURCE_REPO_PATH/$file to $DEST_REPO_PATH"
-  cp $SOURCE_REPO_PATH/$file $DEST_REPO_PATH/
+  cp $SOURCE_REPO_PATH/$file $DEST_REPO_PATH/$file
 done
